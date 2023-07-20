069e04e5a7dd0ec6a5b3011f0dc5b496cffc7af9
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-20 17:23:32 +0200

    sostituzione nomi 'task' con 'card'

--- api/index.php
+++ api/index.php
@@ -1,17 +1,20 @@
 <?php
-$database_path = __DIR__ . '/../database/tasks.json';
+$database_path = __DIR__ . '/../database/cards.json';
 
 $json_data = file_get_contents($database_path);
 
-$tasks = json_decode($json_data);
+$cards = json_decode($json_data, true);
 
-$new_task = $_POST['task'] ?? null;
-if ($new_task) {
-  $tasks[] = $new_task;
-  $json_tasks = json_encode($tasks);
-  file_put_contents($database_path, $json_tasks);
+$new_card = $_POST['card'] ?? null;
+if ($new_card) {
+  $cards[] = $new_card;
+  $json_cards = json_encode($cards);
+  file_put_contents($database_path, $json_cards);
+
+  header('Content-Type: application/json');
+  echo ucfirst($new_card);
+} else {
+
+  header('Content-Type: application/json');
+  echo json_encode($cards);
 }
-
-header('Content-Type: application/json');
-
-echo json_encode($tasks);
--- js/script.js
+++ js/script.js
@@ -6,10 +6,22 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-      tasks: []
+      cards: [],
+      newCard: ''
     }
   },
   created(){
-    axios.get('http://localhost:8888/php-todo-list-json/api/').then(res => { this.tasks = res.data });
+    axios.get('http://localhost:8888/php-todo-list-json/api/').then(res => { this.cards = res.data });
+  },
+  methods: {
+    addCard(){
+      const data = {card: this.newCard};
+
+      const config = {headers: {'Content-Type': 'multipart/form-data'}};
+      axios.post('http://localhost:8888/php-todo-list-json/api/', data, config).then(res => {
+        this.cards.push(res.data);
+        this.newCard = '';
+      })
+    }
   }
 }).mount('#root');
